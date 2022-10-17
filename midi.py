c092a0de96e3c42a853207b7d1faf957ff86c571
tom <clayton_tom@yahooo.com>
2022-10-17 20:05:28 +0100

    midi events

--- midi.py
+++ midi.py
@@ -1,28 +1,24 @@
 
-from alsa_midi import SequencerClient
-from alsa_midi import EventType
+from alsa_midi import SequencerClien, EventType, ControlChangeEvent,\
+                      NonRegisteredParameterChangeEvent, SysExEvent
 
 import threading
 
-STATUS_BIT      = 0x80
-
-MSG_CC          = 0xb0
-MSG_SYSEX       = 0xf0
 MSG_SYSEX_END   = 0xf7
-MSG_NRPN_MSB    = 0x63
-MSG_NRPN_LSB    = 0x62
-MSG_VAL_MSB     = 0x06
-MSG_VAL_LSB     = 0x26
+MSG_PARAM_MSB    = 0x63
+MSG_PARAM_LSB    = 0x62
+MSG_VALUE_MSB     = 0x06
+MSG_VALUE_LSB     = 0x26
 
 class Midi(object):
     def __init__(self, cc_callback=None, sysex_callback=None):
         """Set up midi interface"""
-        self.setup()
+        self._setup()
 
         self.cc_callback = cc_callback
         self.sysex_callback = sysex_callback
-        
-        self.message_data = bytearray()
+
+        self._create_data()
         
         input_thread = threading.Thread(
             target=self.poll, 
@@ -30,106 +26,93 @@ class Midi(object):
         )
         input_thread.start()
     
-    def setup(self):
+    def _setup(self):
         """setup alsa midi"""
         self.client = SequencerClient("Synth Controller")
-        self.port = self.client.create_port("inout")
+        self.port = self.client.create_port("main")
+
+    def _create_data():
+        """create empty received message data arrays"""
+        self.nrpn_data = []
+        self.sysex_data = []
+        for i in range(16):
+            self._clear_channel_data(i)      
+
+    def _clear_channel_data(self, channel):
+        """clear data for given channel"""
+        self.nrpn_data[channel] = {
+                "param_msb": None,
+                "param_lsb": None,
+                "value_msb": None,
+                "value_lsb": None
+            }   
+        self.sysex_data[channel] = None
         
-    def poll(self):
-        """poll midii for input"""
+    def _poll(self):
+        """poll midi for input"""
         print ("thread started")
         while True:
             event = self.client.event_input()
             if event.type == EventType.CONTROLLER:
-                self.parse_cc(event)
+                self._parse_cc(event)
+            elif event.type == EventType.SYSEX:
+                self._parse_sysex(event)
      
-    def parse_cc(self, event):
-        """parce a control change midi message"""
-        if event.param not in (
-            MSG_NRPN_MSB, 
-            MSG_NRPN_LSB, 
-            MSG_VAL_MSB,
-            MSG_VAL_LSB
-        ):
-            pass
-            
+    def _parse_cc(self, event):
+        """parse a control change midi message"""
+        if event.param == MSG_PARAM_MSB:
+            self.data[event.channel]['param_msb'] = event.value
+            self.check_nrpn(event.channel)
+        elif event.param == MSG_PARAM_MSB:
+            self.data[event.channel]['param_lsb'] = event.value
+            self.check_nrpn(event.channel)
+        elif event.param == MSG_VALUE_MSB:
+            self.data[event.channel]['value_msb'] = event.value
+            self.check_nrpn(event.channel)
+        elif event.param == MSG_VALUE_MSB:
+            self.data[event.channel]['value_lsb'] = event.value
+            self.check_nrpn(event.channel)
+
+        else: # standard cc
+            self.cc_callback(event.channel, event.param, event.value) 
         
-    def send_cc(self, channel, controller, value):
-        """send a control change midi midi_data for given values"""
-        midi_out.send_message([MSG_CC | (channel-1), controller, value])
+    def _check_nrpn(channel):
+        """check if all parts of nrpn message have arrived"""
+        if all(self.nrpn_data[channel]):
+            param = self.nrpn_data[channel]['param_msb'] << 7 \
+                    + self.nrpn_data[channel]['param_lsb']
+            value = self.nrpn_data[channel]['value_msb'] << 7 \
+                    + self.nrpn_data[channel]['value_lsb']
 
-    def send_nrpn(self, channel, nrpn, value):
-        """send a control change midi messsage for given values"""
-        midi_out.send_message([
-                MSG_CC | (channel -1),
-                MSG_NRPN_MSB, nrpn >> 7,
-                MSG_NRPN_LSB, nrpn & 0x7f,
-                MSG_VAL_MSB, value >> 7,
-                MSG_VAL_LSB, value & 0x7f
-            ])
-
-    def send_sysex(self, message):
-        """send a system exclusive messsage."""
-        msg = [MSG_SYSEX_START]
-        msg.extend(message)
-        msg.extend(MSG_SYSEX_END)
-        midi_out.send_message(msg)
-
-    def _on_midi(self, data, _):
-        """parse status byte and deal with midi messsage"""
-        message = data[0]
-        print(message)
-        return
-        message_type = message[0] & 0xf0
+            self.cc_callback(self.channel, param, value)
+            self._clear_channel_data(channel)       
         
-    
-        
-        if message & STATUS_BIT:
-            self.message = []
-
-        # parse status byte:
-        message_type = message[0] & 0xf0
-        if message_type in [MSG_CC, MSG_SYSEX]:
-            self.messa
-            
-
-        if message_type == MSG_CC:
-            print("cc")  
-            #self._receive_cc(message)
-        elif message_type == MSG_SYSEX:
-            print("sysex")
-            #self._receive_sysex(message)
-
-    def _receive_cc(data):
-        """receive a control change message"""
-        if data[1] not in [
-                        MSG_NRPN_MSB,
-                        MSG_NRPN_LSB,
-                        MSG_VAL_MSB,
-                        MSG_VAL_LSB,
-                    ]:
-            # Standard CC control change:
-            #self.cc_callback(data[0] & 0x0f, data[1], data[2])
-            print(data, " control change ", data[0] & 0x0f, data[1], data[2])
+    def _parse_sysex(self, event):
+        """parse midi sysex message"""
+        self.channel = event.channel
+        if event.data[-1] != MSG_SYSEX_END:
+            self.sysex_data == event.data
         else:
-            #self.cc_callback(
-            #                data[0] & 0x0f,
-            #                data[1] << 7 | data[2],
-            #                data[3] << 7 | data[4]
-            #            )
-            print(
-                data,
-                " nrpn change ",
-                data[0] & 0x0f,
-                data[1] << 7 | data[2],
-                data[3] << 7 | data[4]
-            )
-               
-    def _receive_sysex(midi_data):
-        """receive a system exclusive message"""
-        print (midi_data)
+            self.sysex_data += event.data
+            self.sysex_callback(self.channel, self.sysex_data)
+            self._clear_channel_data(channel)
 
+    def send_cc(self, channel, controller, value):
+        """send standard control change midi message for given values"""
+        self.client.event_ouput(
+            ControlChangeEvent(channel, controller, value)
+        )
+        client.drain_output()
         
-
+    def send_nrpn(self. channel, controller, value):
+        """send a nrpn control change midi message for given values"""
+        self.client.event_ouput(
+            NonRegisteredParameterChangeEvent(channel, controller, value)
+        )
+        client.drain_output()
         
-
+    def send_sysex(self, data):
+        """send a system exclusive messsage with given data."""
+        self.client.event_ouput(SysExEvent(data))
+        client.drain_output()
+        
